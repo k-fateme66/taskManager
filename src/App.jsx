f7c6a3ce39ea4d355a21633deb00ea71e4c4fec8
fateme karimi <k.fateme66@gmail.com>
2023-10-07 20:39:00 +0330

    Fix bug provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Provider } from "react-redux";
 import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
-import FormShowProvider from "./context/FormShowProvider";
 import store from "./features/store";
 import Archive from "./pages/Archive";
 import Profile from "./pages/Profile";
@@ -13,20 +12,18 @@ import Layout from "./components/Layout/Layout";
 function App() {
   return (
     <Provider store={store}>
-      <FormShowProvider>
-        <Routes>
-          <Route element={<PrivateRoutes />}>
-            <Route element={<Layout />}>
-              <Route path="/" element={<Navigate to="/tasks" />} />
-              <Route path="/tasks" element={<Tasks />} />
-              <Route path="/archives" element={<Archive />} />
-              <Route path="/profile" element={<Profile />} />
-            </Route>
+      <Routes>
+        <Route element={<PrivateRoutes />}>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Navigate to="/tasks" />} />
+            <Route path="/tasks" element={<Tasks />} />
+            <Route path="/archives" element={<Archive />} />
+            <Route path="/profile" element={<Profile />} />
           </Route>
-          <Route path="/login" element={<Registration />} />
-          <Route path="/signup" element={<Registration />} />
-        </Routes>
-      </FormShowProvider>
+        </Route>
+        <Route path="/login" element={<Registration />} />
+        <Route path="/signup" element={<Registration />} />
+      </Routes>
     </Provider>
   );
 }
